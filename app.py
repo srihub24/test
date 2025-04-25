1e09bf7e4b4056877e42c6aac72effd1aeb8bf95
srihub24 <sridevi.maddy.kkl@gmail.com>
2025-04-25 21:14:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import pandas as pd
 import streamlit as st
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
 st.title("Anomaly detection")
 uploaded_file=st.file_uploader("Upload your file here")
 if uploaded_file:
   df=pd.read_csv(uploaded_file)
   st.write(df.describe())
+  features = ['TransactionAmount', 'TransactionDuration', 'AccountBalance', 'LoginAttempts']  # Modify as needed
+  X = df[features].copy()
+  X = X.fillna(X.mean())
+# Standardize the features 
+  scaler = StandardScaler()
+  X_scaled = scaler.fit_transform(X)
+  st.write(X_scaled)
+  iso_forest = IsolationForest(n_estimators=100, contamination=0.05, random_state=42)
+  iso_forest.fit(X_scaled)
+
+# Predict anomalies
+  df['AnomalyScore'] = iso_forest.decision_function(X_scaled)
+  df['IsAnomaly'] = iso_forest.predict(X_scaled)
+  st.write(df[df['IsAnomaly']==-1])
+  
